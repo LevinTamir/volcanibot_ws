8dafff791acd9e2625f8c2cc12f2ad2ac397d0c7
Tamir Levin <tamirlvn@gmail.com>
2025-03-05 16:46:39 +0200

    fix gazebo ign launch file

--- src/volcanibot_description/launch/volcanibot_bringup.launch.py
+++ src/volcanibot_description/launch/volcanibot_bringup.launch.py
@@ -1,7 +1,8 @@
 import os
+from pathlib import Path
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from launch.substitutions import PathJoinSubstitution, Command
@@ -10,27 +11,37 @@ from launch_ros.parameter_descriptions import ParameterValue
 
 
 def generate_launch_description():
-    package_share_directory = FindPackageShare("volcanibot_description").find(
-        "volcanibot_description"
-    )
-    urdf_path = os.path.join(
-        package_share_directory, "urdf", "volcanibot_description.xacro"
-    )
-    meshes_path = os.path.join(package_share_directory, "meshes")
+    volcanibot_description = get_package_share_directory("volcanibot_description")
 
+    # Set Gazebo resource path
+    gazebo_resource_path = SetEnvironmentVariable(
+        name="GZ_SIM_RESOURCE_PATH",
+        value=[str(Path(volcanibot_description).parent.resolve())],
+    )
+
+    # Robot description
     robot_description = ParameterValue(
-        Command(["xacro ", urdf_path, " meshes:=", meshes_path]), value_type=str
+        Command(
+            [
+                "xacro ",
+                os.path.join(
+                    volcanibot_description, "urdf", "volcanibot_description.xacro"
+                ),
+            ]
+        ),
+        value_type=str,
     )
 
+    # Robot State Publisher
     robot_state_publisher_node = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
         name="robot_state_publisher",
         output="screen",
-        parameters=[{"robot_description": robot_description}],
+        parameters=[{"robot_description": robot_description, "use_sim_time": True}],
     )
 
-    # Spawn
+    # Spawn robot in Gazebo Ignition
     spawn_node = Node(
         package="ros_gz_sim",
         executable="create",
@@ -55,20 +66,21 @@ def generate_launch_description():
         output="screen",
     )
 
+    # Launch Gazebo Ignition
     ignition_gazebo_node = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            [
-                PathJoinSubstitution(
-                    [FindPackageShare("ros_gz_sim"), "launch", "gz_sim.launch.py"]
-                )
-            ]
+            PathJoinSubstitution(
+                [FindPackageShare("ros_gz_sim"), "launch", "gz_sim.launch.py"]
+            )
         ),
-        launch_arguments=[("gz_args", [" -r -v 4 empty.sdf"])],
+        launch_arguments=[("gz_args", ["-r -v 4 empty.sdf"])],
     )
 
-    ld = LaunchDescription()
-    ld.add_action(robot_state_publisher_node)
-    ld.add_action(spawn_node)
-    ld.add_action(ignition_gazebo_node)
-
-    return ld
+    return LaunchDescription(
+        [
+            gazebo_resource_path,
+            robot_state_publisher_node,
+            spawn_node,
+            ignition_gazebo_node,
+        ]
+    )

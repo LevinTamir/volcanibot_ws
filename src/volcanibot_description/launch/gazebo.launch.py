d0243ca2dd11834943a9a01d0ddb1e2e75d5bc5b
Tamir Levin <tamirlvn@gmail.com>
2025-03-27 18:27:03 +0200

    add gazebo.launch.py file

---
+++ src/volcanibot_description/launch/gazebo.launch.py
@@ -0,0 +1,120 @@
+import os
+from os import pathsep
+from pathlib import Path
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import (
+    DeclareLaunchArgument,
+    IncludeLaunchDescription,
+    SetEnvironmentVariable,
+)
+from launch.substitutions import (
+    Command,
+    LaunchConfiguration,
+    PathJoinSubstitution,
+    PythonExpression,
+)
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+
+
+def generate_launch_description():
+    volcanibot_description = get_package_share_directory("volcanibot_description")
+
+    model_arg = DeclareLaunchArgument(
+        name="model",
+        default_value=os.path.join(volcanibot_description, "urdf", "volcanibot.xacro"),
+        description="Absolute path to robot urdf file",
+    )
+
+    world_name_arg = DeclareLaunchArgument(name="world_name", default_value="farm")
+
+    world_path = PathJoinSubstitution(
+        [
+            volcanibot_description,
+            "worlds",
+            PythonExpression(
+                expression=["'", LaunchConfiguration("world_name"), "'", " + '.sdf'"]
+            ),
+        ]
+    )
+
+    model_path = str(Path(volcanibot_description).parent.resolve())
+    model_path += pathsep + os.path.join(
+        get_package_share_directory("volcanibot_description"), "models"
+    )
+
+    gazebo_resource_path = SetEnvironmentVariable("GZ_SIM_RESOURCE_PATH", model_path)
+
+    robot_description = ParameterValue(
+        Command(["xacro ", LaunchConfiguration("model")]),
+        value_type=str,
+    )
+
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[{"robot_description": robot_description}],
+    )
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(get_package_share_directory("ros_gz_sim"), "launch"),
+                "/gz_sim.launch.py",
+            ]
+        ),
+        launch_arguments={
+            "gz_args": PythonExpression(["' ", world_path, " -v 4 -r'"])
+        }.items(),
+    )
+
+    gz_spawn_entity = Node(
+        package="ros_gz_sim",
+        executable="create",
+        output="screen",
+        arguments=[
+            "-topic",
+            "robot_description",
+            "-name",
+            "volcanibot",
+            "-x",
+            "2.0",
+            "-y",
+            "2.0",
+            "-z",
+            "0",
+            "-r",
+            "0",
+            "-p",
+            "0",
+            "-Y",
+            "1.57",
+        ],
+    )
+
+    # gz_ros2_bridge = Node(
+    #     package="ros_gz_bridge",
+    #     executable="parameter_bridge",
+    #     arguments=[
+    #         "/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock",
+    #         "/imu@sensor_msgs/msg/Imu[gz.msgs.IMU",
+    #         "/scan@sensor_msgs/msg/LaserScan[gz.msgs.LaserScan",
+    #     ],
+    #     remappings=[
+    #         ("/imu", "/imu/out"),
+    #     ],
+    # )
+
+    return LaunchDescription(
+        [
+            model_arg,
+            world_name_arg,
+            robot_state_publisher_node,
+            gazebo_resource_path,
+            gazebo,
+            gz_spawn_entity,
+            # gz_ros2_bridge,
+        ]
+    )

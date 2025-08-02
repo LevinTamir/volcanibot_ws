21059ed9de41c708483ac393357583d4efba23a2
Tamir Levin <tamirlvn@gmail.com>
2025-08-02 21:25:05 +0300

    Add rgbd camera

--- src/volcanibot_description/launch/gazebo.launch.py
+++ src/volcanibot_description/launch/gazebo.launch.py
@@ -104,6 +104,10 @@ def generate_launch_description():
         executable="parameter_bridge",
         arguments=[
             "/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock",
+            "/camera/camera_info@sensor_msgs/msg/CameraInfo@gz.msgs.CameraInfo",
+            "/camera/image@sensor_msgs/msg/Image@gz.msgs.Image",
+            "/camera/depth_image@sensor_msgs/msg/Image@gz.msgs.Image",
+            "/camera/points@sensor_msgs/msg/PointCloud2@gz.msgs.PointCloudPacked",
         ],
     )
 

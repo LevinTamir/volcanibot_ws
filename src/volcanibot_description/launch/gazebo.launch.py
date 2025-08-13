cba03e1ab003f36d41f4b621f96df6238825b074
Tamir Levin <tamirlvn@gmail.com>
2025-08-13 15:24:11 +0300

    Add feild.sdf file

--- src/volcanibot_description/launch/gazebo.launch.py
+++ src/volcanibot_description/launch/gazebo.launch.py
@@ -35,7 +35,7 @@ def generate_launch_description():
             volcanibot_description,
             "worlds",
             PythonExpression(
-                expression=["'", LaunchConfiguration("world_name"), "'", " + '.world'"]
+                expression=["'", LaunchConfiguration("world_name"), "'", " + '.sdf'"]
             ),
         ]
     )
